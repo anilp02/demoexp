6d25d1c3e50f6d9eb5b070db0c11e5b4fedf7363
anilp02 <anilkumarp31@gmail.com>
2018-06-19 08:07:05 -0400

    initial

---
+++ src/main/java/examplep/com/demoexp/DemoexpApplication.java
@@ -0,0 +1,12 @@
+package examplep.com.demoexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoexpApplication.class, args);
+	}
+}
---
+++ src/test/java/examplep/com/demoexp/DemoexpApplicationTests.java
@@ -0,0 +1,16 @@
+package examplep.com.demoexp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoexpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
